a7a8de9b4ea8a5d3272ffc4ad5fa5137c07e4e77
Julio Romero <julio_ale21@hotmail.com>
2021-06-13 19:18:53 -0300

    chore: added context to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,55 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
-import LoginView from "./screens/LoginView";
+import LoginView from "./screens/login/LoginView";
 import SearchView from "./screens/search/SearchView";
 import Home from "./screens/home/HomeView";
+import { HeroContext } from "./HeroContext";
+import Hero from "./models/Hero";
+import HeroDetailView from "./screens/detail/HeroDetailView";
+
+const initialState = JSON.parse(localStorage.getItem("heroes") || "[]");
 
 const App: React.FC = () => {
+  const [selectedHero, setSelectedHero] = React.useState<Hero>({} as Hero);
+  const [heroes, setHeroes] = React.useState<Hero[]>(initialState);
+
   const token = localStorage.getItem("token");
 
-  /* if (!token) {
-    return (
-      <Router>
-        <div className="App">
-          <LoginView />
-        </div>
-      </Router>
-    );
-  } */
+  React.useEffect(() => {
+    localStorage.setItem("heroes", JSON.stringify(heroes));
+  }, [heroes]);
 
   return (
-    <Router>
-      <div className="App">
-        {!token && <Redirect to="/login" />}
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
+    <HeroContext.Provider value={{ selectedHero, heroes, setSelectedHero, setHeroes }}>
+      <Router>
+        <div className="App">
+          {!token && <Redirect to="/login" />}
+          <Switch>
+            <Route path="/detail">
+              <HeroDetailView />
+            </Route>
 
-          <Route path="/login">
-            {token && <Redirect to="/home" />}
-            <LoginView />
-          </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
 
-          <Route path="/search">
-            <SearchView />
-          </Route>
+            <Route path="/login">
+              {token && <Redirect to="/home" />}
+              <LoginView />
+            </Route>
 
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+            <Route path="/search">
+              <SearchView />
+            </Route>
+
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </HeroContext.Provider>
   );
 };
 
