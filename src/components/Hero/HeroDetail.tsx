2a3c503dd764ed3f667a34aaef970420c18e6e9f
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-06-17 09:43:46 -0300

    chore: added hero detail component

--- src/components/Hero/HeroDetail.tsx
+++ src/components/Hero/HeroDetail.tsx
@@ -8,20 +8,43 @@ interface DetailProps {
 const HeroDetail: React.FC<DetailProps> = ({ hero, onClose }) => {
   return (
     <div className="d-flex position-fixed justify-content-center align-items-center top-0 w-100 h-100">
-      <div className="card-detail bg-black d-flex flex-column flex-md-row justify-content-between">
-        <div>
+      <div className="card-detail bg-black d-flex flex-column">
+        <div className="w-100">
+          <button
+            className="float-end bg-transparent rounded-circle border border-1 border-danger po-1 m-4"
+            onClick={onClose}
+          >
+            <span className="text-danger">X</span>
+          </button>
+        </div>
+        <div className="d-flex justify-content-center align-items-center mx-2">
           <img alt={hero.name} className="img-detail" src={hero.image.url} />
         </div>
         <div className="d-flex flex-column align-items-center p-5 pt-3 pe-3 text-white">
-          <div className="w-100">
-            <button className="float-end" onClick={onClose}>
-              X
-            </button>
-          </div>
           <h1 className="text-white">{hero.name}</h1>
           <p>
             <span className="text-danger fw-bolder me-3">Aliases: </span>
             {hero.biography.aliases.toString()}
+          </p>
+          <p>
+            <span className="text-danger fw-bolder me-3">Weight:</span>
+            {hero.appearance.weight[1]}
+          </p>
+          <p>
+            <span className="text-danger fw-bolder me-3">Height:</span>
+            {hero.appearance.height[1]}
+          </p>
+          <p>
+            <span className="text-danger fw-bolder me-3">Eyes Color:</span>
+            {hero.appearance["eye-color"]}
+          </p>
+          <p>
+            <span className="text-danger fw-bolder me-3">Hair Color:</span>
+            {hero.appearance["hair-color"]}
+          </p>
+          <p>
+            <span className="text-danger fw-bolder me-3">Place of work:</span>
+            {hero.work.base}
           </p>
         </div>
       </div>
