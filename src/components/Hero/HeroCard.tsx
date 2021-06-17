7ded03fdcd9fc3a524ea0b89225e74771c4d6af0
Julio Romero <julio_ale21@hotmail.com>
2021-06-17 00:18:46 -0300

    refactor: added powerstats to hero card

--- src/components/Hero/HeroCard.tsx
+++ src/components/Hero/HeroCard.tsx
@@ -10,16 +10,37 @@ interface HeroCardProps {
 const HeroCard: React.FC<HeroCardProps> = ({ hero, onHeroSelected, onRemoveSelected }) => {
   return (
     <div className="col-10 col-sm-6 col-md-3 col-xl-2 mt-5" data-aos="fade-up">
-      <div style={{ backgroundColor: "rgba(255, 255, 255, 0.1)" }}>
+      <div
+        className="h-100 d-flex flex-column justify-content-between"
+        style={{ backgroundColor: "rgba(255, 255, 255, 0.1)" }}
+      >
         <div className="mt-2 p-2">
           <img alt="hero" className="rounded-2" src={hero.image.url} width="100%" />
         </div>
 
-        <h4 className="mt-2 text-white pb-2">{hero.name}</h4>
-        <p className="text-white text-sm">Alignment: {hero.biography.alignment}</p>
+        <div>
+          <h4 className="mt-2 text-white pb-2">{hero.name}</h4>
+
+          <div className="row">
+            {Object.keys(hero.powerstats).map((key) => (
+              <div key={key} className="col-6">
+                <p className="text-sm text-warning fw-bolder">
+                  <span className="me-2 fw-bold text-capitalize text-white shadow-black">
+                    {key}:
+                  </span>
+                  {hero.powerstats[key]}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
 
         <div className="d-grid gap-1 col-11 mx-auto pb-3">
-          <button className="btn btn-primary btn-sm" type="button" onClick={onHeroSelected}>
+          <button
+            className="btn bg-secondary btn-sm text-white"
+            type="button"
+            onClick={onHeroSelected}
+          >
             View Detail
           </button>
           <button className="btn btn-danger btn-sm" type="button" onClick={onRemoveSelected}>
