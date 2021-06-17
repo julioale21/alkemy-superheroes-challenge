511c774efec0eaa8cdbdefc09b3f4a09af1979d2
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-06-17 09:44:24 -0300

    fix: fixed add hero card size

--- src/components/home/AddHeroCard.tsx
+++ src/components/home/AddHeroCard.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 const AddHeroCard: React.FC<CardProps> = ({ onAddClick }) => {
   return (
     <div
-      className="col-8 col-sm-6 col-md-3 col-xl-2 mt-5 mb-5 mb-sm-0"
+      className="col-8 col-sm-6 col-md-3 col-xl-2 p-1 mt-5 mb-5 mb-sm-0"
       data-aos="fade-up"
       style={{ minHeight: "300px" }}
     >
