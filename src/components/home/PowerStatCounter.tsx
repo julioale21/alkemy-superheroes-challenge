58daea84bb3fdca0e6a1134db78ef359fb8f1524
Julio Romero <julio_ale21@hotmail.com>
2021-06-17 00:20:43 -0300

    chore: updated counter

--- src/components/home/PowerStatCounter.tsx
+++ src/components/home/PowerStatCounter.tsx
@@ -12,19 +12,18 @@ const PowerStatCounter: React.FC<PowerProps> = ({ name, value }) => {
       <h6>
         <span>{name.toUpperCase()}</span>
       </h6>
-      <div className="border border-3 w-circle mb-3 mx-auto p-2 rounded-circle">
-        <CountUp delay={0} end={value} start={0}>
-          {({ countUpRef }) => (
-            <div>
-              <span
-                ref={countUpRef}
-                className="fs-1 text-red fw-bolder"
-                style={{ textShadow: "3px 3px 2px white" }}
-              />
-            </div>
-          )}
-        </CountUp>
-      </div>
+
+      <CountUp delay={0} end={value} start={0}>
+        {({ countUpRef }) => (
+          <div>
+            <span
+              ref={countUpRef}
+              className="fs-1 text-red fw-bolder"
+              style={{ textShadow: "3px 3px 2px white" }}
+            />
+          </div>
+        )}
+      </CountUp>
     </div>
   );
 };
