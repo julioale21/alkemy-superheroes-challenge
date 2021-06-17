c9f4f2b8282edab9f16973c267dffb17cb702278
Julio Romero <julio_ale21@hotmail.com>
2021-06-17 00:21:36 -0300

    feat(detail): user can view a hero detail from home

--- src/screens/home/HomeView.tsx
+++ src/screens/home/HomeView.tsx
@@ -7,9 +7,11 @@ import usePowerStats from "../../components/Hero/usePowerStats";
 import Hero from "../../models/Hero";
 import PowerStatCounter from "../../components/home/PowerStatCounter";
 import "./index.css";
+import HeroDetail from "../../components/Hero/HeroDetail";
 
 const HomeView: React.FC = () => {
-  const { heroes, setHeroes, setSelectedHero } = React.useContext(HeroContext);
+  const { heroes, setHeroes, selectedHero, setSelectedHero } = React.useContext(HeroContext);
+  const [modal, setModal] = React.useState(false);
   const history = useHistory();
 
   const { powerstats, averageWeight, averageHeight, sortedArray } = usePowerStats(heroes);
@@ -20,7 +22,8 @@ const HomeView: React.FC = () => {
 
   const handleSelectedHero = (hero: Hero) => {
     setSelectedHero(hero);
-    history.push("/detail");
+    setModal(true);
+    //history.push("/detail");
   };
 
   const handleRemoveHero = (hero: Hero) => {
@@ -31,24 +34,28 @@ const HomeView: React.FC = () => {
     <div className="h-100 d-flex flex-column align-items-center">
       <div className="powerstats-sections w-100 min-vh-100 d-flex flex-column align-items-center justify-content-around text-white">
         <h2 className="text-uppercase fw-bold">Powerstats</h2>
-        <div className="row w-100">
-          {Object.keys(sortedArray).map((key) => (
-            <PowerStatCounter key={key} name={key} value={powerstats[key]} />
-          ))}
-        </div>
-        <div className="averages w-100 d-flex flex-row justify-content-around">
-          <div data-aos="fade-up">
-            <h3>Weight</h3>
-            <p>{averageWeight.toFixed(2)} Kg</p>
+        <div className="d-flex flex-column flex-md-row">
+          <div className="row w-100">
+            {Object.keys(sortedArray).map((key) => (
+              <PowerStatCounter key={key} name={key} value={powerstats[key]} />
+            ))}
           </div>
-          <div data-aos="fade-up">
-            <h3>Height</h3>
-            <p>{averageHeight.toFixed(2)} cm</p>
+          <div className="averages w-100 d-flex flex-row flex-md-column justify-content-around">
+            <div data-aos="fade-up">
+              <h6>average</h6>
+              <h2>Weight</h2>
+              <p className="fs-1">{averageWeight.toFixed(2)} Kg</p>
+            </div>
+            <div data-aos="fade-up">
+              <h6>average</h6>
+              <h2>Height</h2>
+              <p className="fs-1">{averageHeight.toFixed(2)} cm</p>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="my-team-section h-100 w-100 min-vh-100 bg-black">
+      <div className="my-team-section h-100 w-100 min-vh-100 bg-black pb-5">
         <div className="row w-100 mx-auto d-flex justify-content-center ">
           {heroes.map((hero) => (
             <HeroCard
@@ -62,6 +69,8 @@ const HomeView: React.FC = () => {
           {heroes.length < 6 && <AddHeroCard onAddClick={handleAddClick} />}
         </div>
       </div>
+
+      {modal ? <HeroDetail hero={selectedHero} onClose={() => setModal(false)} /> : null}
     </div>
   );
 };
