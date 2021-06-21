9d971b29af95746fa7de19415f4a2c5acd5010d7
Julio Romero <julio_ale21@hotmail.com>
2021-06-21 16:17:47 -0300

    chore: extracted logic to a custom hooks

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import InlineRadioButton from "../shared/InlineRadioButton";
+import searchLogo from "../../assets/search2.png";
 
 interface FormProps {
   onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onFormSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  onCheckButtonChanged: (e: React.FormEvent<HTMLInputElement>) => void;
+  checkValue: string;
+  value: string;
 }
 
-const SearchForm: React.FC<FormProps> = ({ onInputChange, onFormSubmit }) => {
+const SearchForm: React.FC<FormProps> = ({
+  onInputChange,
+  onFormSubmit,
+  onCheckButtonChanged,
+  checkValue,
+  value,
+}) => {
   return (
     <form className="form-search" onSubmit={(e) => onFormSubmit(e)}>
       <div className="row">
+        <div className="mb-4">
+          <img alt="search" src={searchLogo} width="160px" />
+        </div>
         <div className="col-10 col-md-4 mx-auto">
           <div className="input-group input-group-lg">
             <input
@@ -17,47 +31,31 @@ const SearchForm: React.FC<FormProps> = ({ onInputChange, onFormSubmit }) => {
               className="form-control"
               placeholder="Search your hero"
               type="text"
+              value={value}
               onChange={onInputChange}
             />
           </div>
 
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              id="inlineRadio1"
-              name="inlineRadioOptions"
-              type="radio"
-              value="option1"
-            />
-            <label className="form-check-label text-white" htmlFor="inlineRadio1">
-              Align All
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              id="inlineRadio2"
-              name="inlineRadioOptions"
-              type="radio"
-              value="option2"
-            />
-            <label className="form-check-label text-white" htmlFor="inlineRadio2">
-              Align Good
-            </label>
-          </div>
+          <InlineRadioButton
+            isChecked={checkValue === "all"}
+            placeHolder="Align All"
+            value="all"
+            onRadioSelected={onCheckButtonChanged}
+          />
 
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              id="inlineRadio3"
-              name="inlineRadioOptions"
-              type="radio"
-              value="option3"
-            />
-            <label className="form-check-label text-white" htmlFor="inlineRadio3">
-              Align Bad
-            </label>
-          </div>
+          <InlineRadioButton
+            isChecked={checkValue === "good"}
+            placeHolder="Align Good"
+            value="good"
+            onRadioSelected={onCheckButtonChanged}
+          />
+
+          <InlineRadioButton
+            isChecked={checkValue === "bad"}
+            placeHolder="Align Bad"
+            value="bad"
+            onRadioSelected={onCheckButtonChanged}
+          />
         </div>
       </div>
       <button className="btn btn-warning mt-3">Buscar</button>
--- src/screens/home/HomeView.tsx
+++ src/screens/home/HomeView.tsx
@@ -1,29 +1,39 @@
 import React from "react";
-import { HeroContext } from "../../HeroContext";
+import { HeroContext } from "../../Context/HeroContext";
 import { useHistory } from "react-router-dom";
-import AddHeroCard from "../../components/home/AddHeroCard";
-import HeroCard from "../../components/Hero/HeroCard";
-import usePowerStats from "../../components/Hero/usePowerStats";
-import Hero from "../../models/Hero";
-import PowerStatCounter from "../../components/home/PowerStatCounter";
-import "./index.css";
+import PowerStatCounter from "../../components/Hero/PowerStatsItems/PowerStatCounter";
+import usePowerStats from "../../hooks/usePowerStats";
+import AddHeroCard from "../../components/Hero/AddHeroCard";
 import HeroDetail from "../../components/Hero/HeroDetail";
+import HeroCard from "../../components/Hero/HeroCard";
+import Hero from "../../models/Hero";
+import "./index.css";
 
 const HomeView: React.FC = () => {
-  const { heroes, setHeroes, selectedHero, setSelectedHero } = React.useContext(HeroContext);
+  const {
+    heroes,
+    setHeroes,
+    selectedHero,
+    setSelectedHero,
+    setSearchResult,
+    setSearchText,
+    setCheckValue,
+  } = React.useContext(HeroContext);
   const [modal, setModal] = React.useState(false);
   const history = useHistory();
 
   const { powerstats, averageWeight, averageHeight, sortedArray } = usePowerStats(heroes);
 
   const handleAddClick = () => {
+    setCheckValue("all");
+    setSearchResult([]);
+    setSearchText("");
     history.push("/search");
   };
 
   const handleSelectedHero = (hero: Hero) => {
     setSelectedHero(hero);
     setModal(true);
-    //history.push("/detail");
   };
 
   const handleRemoveHero = (hero: Hero) => {
@@ -31,31 +41,39 @@ const HomeView: React.FC = () => {
   };
 
   return (
-    <div className="h-100 d-flex flex-column align-items-center">
-      <div className="powerstats-sections w-100 min-vh-100 d-flex flex-column align-items-center justify-content-around text-white">
-        <h2 className="text-uppercase fw-bold">Powerstats</h2>
-        <div className="d-flex flex-column flex-md-row">
-          <div className="row w-100">
-            {Object.keys(sortedArray).map((key) => (
-              <PowerStatCounter key={key} name={key} value={powerstats[key]} />
-            ))}
-          </div>
-          <div className="averages w-100 d-flex flex-row flex-md-column justify-content-around">
-            <div data-aos="fade-up">
-              <h6>average</h6>
-              <h2>Weight</h2>
-              <p className="fs-1">{averageWeight.toFixed(2)} Kg</p>
+    <div className="w-100 overflow-hidden">
+      <div className="powerstats-sections min-vh-100">
+        <div
+          className="powerstats-info d-flex flex-column min-vh-100 justify-content-around align-items-center"
+          data-aos="fade-up"
+          data-aos-delay="200"
+          data-aos-duration="2000"
+        >
+          <h1 className="title text-white fw-bolder text-uppercase">Super Heroes</h1>
+
+          {heroes.length && (
+            <div>
+              <div className="powerstats row">
+                {Object.keys(sortedArray).map((key) => (
+                  <PowerStatCounter key={key} name={key} value={powerstats[key]} />
+                ))}
+              </div>
+              <div className="averages w-100 d-flex flex-row justify-content-around">
+                <div data-aos="fade-up">
+                  <p className="fs-4 m-0 text-white">{averageWeight.toFixed(2)} Kg</p>
+                  <p className="text-sm m-0 text-white">Average Weight</p>
+                </div>
+                <div data-aos="fade-up">
+                  <p className="fs-4 m-0 text-white">{averageHeight.toFixed(2)} cm</p>
+                  <p className="text-sm m-0 text-white">Average height</p>
+                </div>
+              </div>
             </div>
-            <div data-aos="fade-up">
-              <h6>average</h6>
-              <h2>Height</h2>
-              <p className="fs-1">{averageHeight.toFixed(2)} cm</p>
-            </div>
-          </div>
+          )}
         </div>
       </div>
 
-      <div className="my-team-section h-100 w-100 min-vh-100 bg-black pb-5">
+      <div className="my-team-section h-100 min-vh-100 pb-5">
         <div className="row w-100 mx-auto d-flex justify-content-center ">
           {heroes.map((hero) => (
             <HeroCard
