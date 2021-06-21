b6f8d287b9298bb197fd4e1b10abf36031f468fe
Julio Romero <julio_ale21@hotmail.com>
2021-06-21 16:21:54 -0300

    chore: updated hero detail view

--- src/screens/HeroDetailView/HeroDetailView.tsx
+++ src/screens/HeroDetailView/HeroDetailView.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
+import { useToasts } from "react-toast-notifications";
+
 import Appearance from "../../components/Hero/PowerStatsItems/Appearance";
 import Biography from "../../components/Hero/PowerStatsItems/Biography";
 import Connections from "../../components/Hero/PowerStatsItems/Connections";
-
 import PowerStats from "../../components/Hero/PowerStatsItems/Powerstats";
 import bgImage from "../../assets/bg-12.jpg";
 import Work from "../../components/Hero/PowerStatsItems/Work";
@@ -12,8 +13,10 @@ import Hero from "../../models/Hero";
 import "./index.css";
 
 const HeroDetailView: React.FC = () => {
-  const { selectedHero, heroes, setHeroes } = React.useContext(HeroContext);
+  const { selectedHero, heroes, setHeroes, setCheckValue, setSearchResult, setSearchText } =
+    React.useContext(HeroContext);
   const history = useHistory();
+  const { addToast } = useToasts();
 
   const exists = (hero: Hero): Boolean => {
     return heroes.findIndex((item) => item.id === hero.id) !== -1;
@@ -21,8 +24,7 @@ const HeroDetailView: React.FC = () => {
 
   const addHero = () => {
     if (heroes.length >= 6) {
-      // eslint-disable-next-line no-console
-      console.log("To many heroes");
+      addToast("Your team is complete!", { appearance: "error" });
 
       return;
     }
@@ -32,28 +34,32 @@ const HeroDetailView: React.FC = () => {
     );
 
     if (alignments.length >= 3) {
-      // eslint-disable-next-line no-console
-      console.log("Alignment limit reached");
+      addToast(`You have reached the limit of ${selectedHero.biography.alignment} alignments`, {
+        appearance: "error",
+      });
 
       return;
     }
 
     if (!exists(selectedHero)) {
       setHeroes([...heroes, selectedHero]);
+      setCheckValue("all");
+      setSearchResult([]);
+      setSearchText("");
       history.push("/home");
     }
   };
 
   return (
     <div
-      className="d-flex justify-content-center align-items-center"
+      className="min-vh-100 d-flex justify-content-center align-items-center"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-      {selectedHero ? (
+      {Object.entries(selectedHero).length > 0 ? (
         <div className="row d-flex justify-content-center align-items-center py-5">
           <div className="hero-short-info col-11 col-sm-8 col-md-6 col-lg-3 col-xl-4 m-0 h-100 fw-bolder p-3 mx-3 text-white">
             <img src={selectedHero.image.url} style={{ width: "100%" }} />
-            <PowerStats powerstats={selectedHero.powerstats} />
+            <PowerStats counterSize={3} powerstats={selectedHero.powerstats} />
           </div>
           <div className="hero-full-info col-11 col-lg-6 p-3 mt-4 mt-lg-0 text-white">
             <h1>{selectedHero.name}</h1>
@@ -81,11 +87,13 @@ const HeroDetailView: React.FC = () => {
                   Cancel
                 </button>
               </div>
-            ) : null}
+            ) : (
+              <p className="fs-6 mt-2 text-danger">Hero is already in your team!</p>
+            )}
           </div>
         </div>
       ) : (
-        <p>There are not hero selected</p>
+        <Redirect to="/home" />
       )}
     </div>
   );
