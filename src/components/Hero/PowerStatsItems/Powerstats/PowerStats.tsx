79ffdad391d138eb5206ed68f02fbb7dcd9846e4
Julio Romero <julio_ale21@hotmail.com>
2021-06-21 16:22:26 -0300

    chore: updated powerStats component

--- src/components/Hero/PowerStatsItems/Powerstats/PowerStats.tsx
+++ src/components/Hero/PowerStatsItems/Powerstats/PowerStats.tsx
@@ -3,15 +3,23 @@ import Powerstats from "../../../../models/Popwerstats";
 import PowerStatCounter from "../PowerStatCounter";
 interface PowerStatListProps {
   powerstats: Powerstats;
+  counterSize?: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
-const PowerStats: React.FC<PowerStatListProps> = ({ powerstats }) => {
+const PowerStats: React.FC<PowerStatListProps> = ({ powerstats, counterSize = 6 }) => {
   return (
     <div>
-      <h4>Powerstats</h4>
+      <h4 className="text-warning">Powerstats</h4>
       <div className="row">
         {Object.keys(powerstats).map((key) => {
-          return <PowerStatCounter key={key} counterSize={6} name={key} value={powerstats[key]} />;
+          return (
+            <PowerStatCounter
+              key={key}
+              counterSize={counterSize}
+              name={key}
+              value={powerstats[key]}
+            />
+          );
         })}
       </div>
     </div>
