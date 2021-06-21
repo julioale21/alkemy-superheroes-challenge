8343058f2f561c8fdd7269d483ef62e4709b9df0
Julio Romero <julio_ale21@hotmail.com>
2021-06-21 08:56:32 -0300

    chore: updated TextInfo component

--- src/components/shared/TextInfo/TextInfo.tsx
+++ src/components/shared/TextInfo/TextInfo.tsx
@@ -2,15 +2,23 @@ import React from "react";
 
 interface TextInfoProps {
   title: string;
-  value: string;
+  value: string | string[];
+  textAlign?: "center" | "start" | "end";
   titleColor?: "primary" | "secondary" | "success" | "danger" | "warning";
+  textSm?: boolean;
 }
-const TextInfo: React.FC<TextInfoProps> = ({ title, value, titleColor = "warning" }) => {
+const TextInfo: React.FC<TextInfoProps> = ({
+  title,
+  value,
+  titleColor = "warning",
+  textAlign = "center",
+  textSm = false,
+}) => {
   return (
     <div>
-      <p>
-        <span className={`text-${titleColor} fw-bolder me-3`}>{title}:</span>
-        {value}
+      <p className={`text-${textAlign} ${textSm && "text-sm"}`}>
+        <span className={`text-${titleColor} fw-bolder me-3 text-capitalize`}>{title}:</span>
+        {value.toString()}
       </p>
     </div>
   );
