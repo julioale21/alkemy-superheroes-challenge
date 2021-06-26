1e1e9365cbf8e59f288655b80fe34440c5f00b49
Julio Romero <julio_ale21@hotmail.com>
2021-06-26 10:44:15 -0300

    style: extracted login page styles

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import LoginForm from "../../components/LoginForm";
-import backgroundImg from "../../assets/bg-14.jpg";
 import useSession from "../../hooks/useSession";
+import "./index.css";
 
 const Login: React.FC = () => {
   const { error, loginUser, loading } = useSession();
 
   return (
-    <div
-      className="mh-100 d-flex justify-content-center align-items-center"
-      style={{
-        backgroundImage: `url(${backgroundImg})`,
-        backgroundSize: "cover",
-        height: "100vh",
-      }}
-    >
+    <div className="login-section mh-100 d-flex justify-content-center align-items-center">
       <LoginForm error={error} loading={loading} login={loginUser} />
     </div>
   );
