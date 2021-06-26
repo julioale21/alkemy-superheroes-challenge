0746f0b367ebf235be67c3a217f1aaaf21f130b9
Julio Romero <julio_ale21@hotmail.com>
2021-06-26 18:43:18 -0300

    chore: replaced couterup by use-counter

--- src/components/Hero/PowerStatsItems/PowerStatCounter/PowerStatCounter.tsx
+++ src/components/Hero/PowerStatsItems/PowerStatCounter/PowerStatCounter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CountUp from "react-countup";
+import { CountUp } from "use-count-up";
 import "./index.css";
 
 interface PowerProps {
@@ -11,11 +11,9 @@ interface PowerProps {
 const PowerStatCounter: React.FC<PowerProps> = ({ name, value, counterSize = 1 }) => {
   return (
     <div className="col-4 mb-3 d-flex flex-column">
-      <CountUp delay={0} end={value} start={0}>
-        {({ countUpRef }) => (
-          <span ref={countUpRef} className={`counter fs-${counterSize} fw-bolder`} />
-        )}
-      </CountUp>
+      <p className={`counter fs-${counterSize} fw-bolder`}>
+        <CountUp isCounting duration={3.2} end={value} start={0} />
+      </p>
       <h6 className="text-sm text-cente text-white">
         {`Total`}
         <span className="text-capitalize text-white fw-bolder"> {name} </span>
